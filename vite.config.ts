1e46d49ea013f051cedc0cc9798105550e0c2566
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-11-04 00:12:50 +0700

    fix path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import solidPlugin from "vite-plugin-solid";
 import svgString from "./plugins/svgString";
 
 export default defineConfig({
+	base: "./",
 	plugins: [solidPlugin(), svgString()],
 	server: {
 		port: 3000,
